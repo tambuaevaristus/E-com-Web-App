ad5fa1e2818d533c9a1b3993fd371fd04c9d4b29
Tambua Evaristus <evaristustambua@gmail.com>
2023-01-18 13:14:11 +0100

    Navigation routes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -31,12 +31,12 @@ export default function Navbar(props: Props) {
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <a className="nav-link" href="/features">
                 Features
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <a className="nav-link" href="/pricing">
                 Pricing
               </a>
             </li>
