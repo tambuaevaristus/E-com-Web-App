5ef743188c849f8a08ba7c60c513d4ddde8928ac
Tambua Evaristus <evaristustambua@gmail.com>
2023-01-21 09:26:11 +0100

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React, {FC} from 'react'
 export const Footer: FC = () => {
   return (
     <div>
-         The footer section
+         <div className="">footer section</div>
     </div>
   )
 }
