06339e3fee12be4abc80e4c54986f73798e6bc55
Tambua Evaristus <evaristustambua@gmail.com>
2023-02-03 15:09:18 +0100

    trainee work

---
+++ firebaseConfig.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBtN2AIiLrcDlTsxHI65LVAWx4uErE1xx4",
+  authDomain: "todo-208e5.firebaseapp.com",
+  projectId: "todo-208e5",
+  storageBucket: "todo-208e5.appspot.com",
+  messagingSenderId: "934609200594",
+  appId: "1:934609200594:web:daf4ec5171c189f69cd40b",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+
