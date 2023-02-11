04d0d214f3dc9a8b762ce980e0cb9fd128197107
Tambua Evaristus <evaristustambua@gmail.com>
2023-02-11 10:14:22 +0100

    read items

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,18 +1,24 @@
-import React from 'react'
+import React from "react";
+interface Props {
+  text: string;
+  status: boolean;
+}
 
-
-
-export const Task = () => {
-
-
- 
+export const Task = ({ text, status }: Props) => {
+  
   return (
     <div>
-        <div className="flex mb-4 items-center border p-3 bg-gray-500">
-                <p className="w-full text-grey-darkest">Add another component to Tailwind Components</p>
-                <button className="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-green border-green hover:bg-green">Done</button>
-                <button className="flex-no-shrink p-2 ml-2 border-2 rounded text-red border-red hover:text-white hover:bg-red">Remove</button>
-            </div>
+      <div className="flex mb-4 items-center border p-3 bg-gray-500">
+        <p className="w-full text-grey-darkest">
+         {text}
+        </p>
+        <button className="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-green border-green hover:bg-green">
+          Done
+        </button>
+        <button className="flex-no-shrink p-2 ml-2 border-2 rounded text-red border-red hover:text-white hover:bg-red">
+          Remove
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
--- src/components/TodoContent.tsx
+++ src/components/TodoContent.tsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import { Task } from './Task'
+import {
+  collection,
+  DocumentData,
+  getDocs,
+  QuerySnapshot,
+} from "firebase/firestore";
+import React, { useState } from "react";
+import { db } from "../firebaseConfig";
+import { Task } from "./Task";
 
 export const TodoContent = () => {
+  const q = collection(db, "todos");
+  const [todoItems, setTodoItems] = useState<any>();
+
+  const getData = async () => {
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      // console.log(doc.id, " => ", doc.data());
+    });
+    setTodoItems(querySnapshot.docs);
+    console.log("This are todo items  =>", todoItems)
+
+    
+
+  };
+
+
+  console.log(todoItems);
+
+  getData();
   return (
     <div>
-        <Task />
+      {todoItems.map(() => {
+        // console.log(doc.id, " => ", doc.data())
+        <Task key={doc.id} text="The first todo item" status={true} />;
+      })}
     </div>
-  )
-}
+  );
+};
